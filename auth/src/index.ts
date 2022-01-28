6028ee74da7f6e741e3542772e7111a162c79740
jduarte <jduarte0912@gmail.com>
2022-01-28 08:44:40 -0800

    Update index.ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,33 +3,30 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-<<<<<<< HEAD
-  console.log('Starting up......');
-=======
-  console.log('Starting up...');
->>>>>>> 53568707ce42e723d78235c77ae3d2fc3373c1e2
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT must be defined');
-  }
+	console.log('Starting up...');
 
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT must be defined');
+	}
 
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    });
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.error(err);
-  }
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
 
-  app.listen(3000, () => {
-    console.log('Listening on port 3000!!');
-  });
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		});
+		console.log('Connected to MongoDb');
+	} catch (err) {
+		console.error(err);
+	}
+
+	app.listen(3000, () => {
+		console.log('Listening on port 3000!!');
+	});
 };
 
 start();
